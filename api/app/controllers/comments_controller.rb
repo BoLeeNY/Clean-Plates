6a557eb64270ccc8363d03256b9e80fea710c239
Bo Lee <boleesr@gmail.com>
2018-09-05 14:12:59 -0400

    working create for comments

--- api/app/controllers/comments_controller.rb
+++ api/app/controllers/comments_controller.rb
@@ -14,4 +14,26 @@ class CommentsController < ApplicationController
         id = params[:id]
         render json: { comments: Comment.find(id) }, include: :restaurant
     end
+
+    def create
+        camis = params[:restaurant_id]
+        if (camis)
+        @comment = Restaurant.find_by({camis: camis}).comments.new(comment_params)
+        else
+        @comment = Comment.new(comment_params)
+        end
+        if @comment.save
+            render json: { comment: @comment }
+        else
+            render json: { message: "Oops", errors: @comment.error }, status: :bad_request
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:data)
+            .require(:attributes)
+            .permit(:name, :comment, :restaurant_id)
+    end
 end
