edb8da2c52eeadc6801475b6beb8c3ac43adc1f6
Bo Lee <boleesr@gmail.com>
2018-09-05 13:58:01 -0400

    finished show for comments

--- api/app/controllers/comments_controller.rb
+++ api/app/controllers/comments_controller.rb
@@ -1,6 +1,17 @@
 class CommentsController < ApplicationController
 
     def index
-        render json: { comments: Comment.all }, include: :restaurant
+        camis = params[:restaurant_id]
+        if (camis)
+        @comments = Restaurant.find_by({camis: camis}).comments
+        else
+        @comments = Comment.all
+        end
+        render json: { comments: @comments }, include: :restaurant
+    end
+
+    def show
+        id = params[:id]
+        render json: { comments: Comment.find(id) }, include: :restaurant
     end
 end
--- api/app/controllers/restaurants_controller.rb
+++ api/app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
     
     def index
-        render json: { restaurants: Restaurant.all }, include: :violations
+        render json: { restaurants: Restaurant.all }, include: :violations, include: :comments
     end
 
     def show
         id = params[:id]
-        render json: { restaurants: Restaurant.where({camis: id})}, include: :violations
+        render json: { restaurants: Restaurant.where({camis: id})}, include: :violations, include: :comments
     end
 end
